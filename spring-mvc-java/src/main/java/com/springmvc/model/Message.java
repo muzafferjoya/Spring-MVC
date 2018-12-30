9bf6a11ee446a077ff059911a18e77252c1cc44e
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2018-12-30 16:10:24 +0530

    Spring-MVC-Java

---
+++ spring-mvc-java/src/main/java/com/springmvc/model/Message.java
@@ -0,0 +1,15 @@
+package com.springmvc.model;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
---
+++ spring-mvc-java/src/main/java/com/springmvc/web/BeanB.java
@@ -0,0 +1,11 @@
+package com.springmvc.web;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanB {
+
+    public BeanB() {
+        super();
+    }
+}
